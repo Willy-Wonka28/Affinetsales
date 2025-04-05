0109187bb3b4f1dec2c0834cc70d4e3d33290948
Willy-Wonka28 <adoledavid345@gmail.com>
2025-04-06 00:05:23 +0100

    Deployment bugs

--- src/client/supabase.ts
+++ src/client/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const projectUrl = import.meta.env.VITE_PROJECT_URL;
-const projectKey = import.meta.env.VITE_PROJECT_KEY;
-
+const projectUrl = import.meta.env.VITE_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
+const projectKey = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(projectUrl, projectKey);
